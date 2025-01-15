d001c640f56325807c7ac7cffe023638d6410f5b
Chenry <henrychen0214@gmail.com>
2025-01-15 16:07:56 -0500

    changed rule and fixed homepage bug

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
-import Link from 'next/link';
 import Image from 'next/image';
 
 
@@ -24,25 +23,25 @@ function Navbar() {
             <li className={`relative text-right ${pathname === '/' ? 'text-mygreen' : 'text-black'}`}
                 onMouseEnter={() => setHovered("experience")}
                 style={hovered === "experience" ? hoverStyle : undefined}>
-                <Link href="/">Experience</Link>
+                <a href="/">Experience</a>
             </li>
             <li className={`text-right ${pathname === '/about' ? 'text-mygreen' : 'text-black'}`}
                 onMouseEnter={() => setHovered("about")}
                 style={hovered === "about" ? hoverStyle : undefined}>
-                <Link href="/about">About</Link>
+                <a href="/about">About</a>
             </li>
             <li className={`text-right ${pathname === '/projects' ? 'text-mygreen' : 'text-black'}`}
                 onMouseEnter={() => setHovered("projects")}
                 style={hovered === "projects" ? hoverStyle : undefined}>
-                <Link href="/projects">Projects</Link>
+                <a href="/projects">Projects</a>
             </li>
             <li className={`flex flex-row items-center ${pathname === '/resume' ? 'text-mygreen' : 'text-mygray'} hover:text-black`}
                 onMouseEnter={() => setHovered("resume")}
                 style={hovered === "resume" ? hoverStyle : undefined}>
-                <Link href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center">
                     <p className="text-right pr-1">Resume</p>
-                    <Image src="/resume.svg" alt="Download resume" className="w-5 h-5" style={{ filter: hovered === "resume" ? 'none' : 'grayscale(100%) brightness(0) invert(0.7)' }} />
-                </Link>
+                    <Image src="/resume.svg" alt="Download resume" width={20} height={20} style={{ filter: hovered === "resume" ? 'none' : 'grayscale(100%) brightness(0) invert(0.7)' }} />
+                </a>
             </li>
         </ul>
     );
